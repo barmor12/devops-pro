2bedff76bb99c3bec248385ac80fe88940adec8d
barmor12 <77288623+barmor12@users.noreply.github.com>
2023-05-29 20:36:18 +0300

    project update

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
-const express = require('express')
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-app.get('/', (req,res)=>{
-    res.send('Hello')
-})
-app.get('/test', (req,res)=>{
-    res.send('test Hello')
-})
+app.use(express.json());
 
-module.exports=app
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.get('/test', (req, res) => {
+  res.send('test Hello');
+});
+
+app.post('/register', (req, res) => {
+  const { name, scores } = req.body;
+  const isValid = scores.every((score) => score >= 0 && score <= 100);
+
+  if (!isValid) {
+    res.status(400).json({ error: 'Scores must be between 0 and 100' });
+    return;
+  }
+
+  const data = {
+    name,
+    scores,
+  };
+
+  const filePath = path.join(__dirname, 'data.txt');
+
+  fs.appendFile(filePath, JSON.stringify(data) + '\n', (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Failed to register' });
+    } else {
+      res.status(201).json({ message: 'Registration successful' });
+    }
+  });
+});
+
+module.exports = app;
