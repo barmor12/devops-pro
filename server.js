d388db041b57db3992c7c98646facefa4d7ba1b6
barmor12 <77288623+barmor12@users.noreply.github.com>
2023-05-29 21:11:36 +0300

    update

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const mongoose = require('./db');
 
 const app = express();
 
 app.use(express.json());
+
+app.post('/api/register', (req, res) => {
+  // Extract the name and scores from the request body
+  const { name, scores } = req.body;
+
+  // Here, you can save the registration data to the MongoDB database using Mongoose
+  // Create a new schema and model for the registration data, then save the document
+
+  // Example code to save the data using a schema named 'Registration'
+  const Registration = mongoose.model('Registration', {
+    name: String,
+    scores: [Number],
+  });
+
+  const registration = new Registration({ name, scores });
+  registration
+    .save()
+    .then(() => {
+      console.log('Registration saved to MongoDB');
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.error('Failed to save registration:', error);
+      res.sendStatus(500);
+    });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello');
@@ -12,32 +37,6 @@ app.get('/', (req, res) => {
 
 app.get('/test', (req, res) => {
   res.send('test Hello');
-});
-
-app.post('/register', (req, res) => {
-  const { name, scores } = req.body;
-  const isValid = scores.every((score) => score >= 0 && score <= 100);
-
-  if (!isValid) {
-    res.status(400).json({ error: 'Scores must be between 0 and 100' });
-    return;
-  }
-
-  const data = {
-    name,
-    scores,
-  };
-
-  const filePath = path.join(__dirname, 'data.txt');
-
-  fs.appendFile(filePath, JSON.stringify(data) + '\n', (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Failed to register' });
-    } else {
-      res.status(201).json({ message: 'Registration successful' });
-    }
-  });
 });
 
 module.exports = app;
