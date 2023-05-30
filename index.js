e5acba85f9321e39939676696732818049a4a208
barmor12 <77288623+barmor12@users.noreply.github.com>
2023-05-30 13:37:25 +0300

    ..

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const app = require('./server');
+const express = require('express');
+const mongoose = require('mongoose');
 
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log('Server started!');
+// MongoDB connection string
+const mongoURI = 'mongodb+srv://barmo2:1Q2w3e123@devops.dyvv7g5.mongodb.net/';
+
+// Connect to MongoDB
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB connected');
+    // Start the server after successful MongoDB connection
+    app.listen(port, () => {
+      console.log('Server started!');
+    });
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
+
+// Define your routes and middleware here
+// ...
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
 });
+
+// Start the server
+// app.listen(port, () => {
+//   console.log(`Server started on port ${port}`);
+// });
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('./server');
+const app = require('./server.js');
 
 describe('Server', () => {
   it('should return "Hello" for the root route', async () => {
